b6b2e801fcfe457bdb1f7029cef7f77f4233ffcd
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2025-03-14 09:21:56 +0900

    rename: study 파일로 이동

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/ParkSohyunee/go-study/greetings"
+	"github.com/ParkSohyunee/go-study/study"
 )
 
 
@@ -13,34 +14,5 @@ func main()  {
     message := greetings.Hello("sohyun")
     fmt.Println(message)
 
-    // 1. Constant
-    const name string = "sohyun"
-    fmt.Println(name)
-
-    // const (
-    //   Visa = "Visa"
-    //   Master = "MasterCard"
-    //   Amex = "American Express"
-    // )
-
-    // 2. Variables
-    var name1 string = "sohyun"
-    var age int
-    age = 100
-    fmt.Println(name1, age)
-
-    // Short Assignment Statement (함수 내부에서, 변수에서만)
-    // 타입 자동 설정
-    name2 := "sohyun"
-    fmt.Println(name2)
-
-    // 여러 변수 선언 및 초기화
-    var one, two, three = true, "sohyun", 1
-    fmt.Println(one, two, three)
-
-    // Boolean
-    // error would be occurred
-    // if true == 1 {
-    //   fmt.Println("value of true is 1")
-    // } 
+    study.VariablesExample()
 }
---
+++ study/variables.go
@@ -0,0 +1,37 @@
+package study
+
+import "fmt"
+
+func VariablesExample(){
+	    // 1. Constant
+		const name string = "sohyun"
+		fmt.Println(name)
+	
+		const (
+		  Visa = "Visa"
+		  Master = "MasterCard"
+		  Amex = "American Express"
+		)
+    fmt.Println(Visa, Master, Amex)
+	
+		// 2. Variables
+		var name1 string = "sohyun"
+		var age int
+		age = 100
+		fmt.Println(name1, age)
+	
+		// Short Assignment Statement (함수 내부에서, 변수에서만)
+		// 타입 자동 설정
+		name2 := "sohyun"
+		fmt.Println(name2)
+	
+		// 여러 변수 선언 및 초기화
+		var one, two, three = true, "sohyun", 1
+		fmt.Println(one, two, three)
+	
+		// Boolean
+		// error would be occurred
+		// if true == 1 {
+		//   fmt.Println("value of true is 1")
+		// } 
+}
